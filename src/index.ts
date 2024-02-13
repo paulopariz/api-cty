38a05aa077b8f9cb8c73371e738267611c8ffa7c
paulopariz <paulopariz01@gmail.com>
2024-02-13 15:35:08 -0300

    feat: variables and build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from "./server/server";
 
-server.listen(3333, () => console.log("App"));
+server.listen(process.env.PORT || 3333, () =>
+  console.log(`App na porta -> ${process.env.PORT || 3333}`)
+);
