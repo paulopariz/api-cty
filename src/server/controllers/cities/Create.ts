7421f5e7d32cfc153ee0bf5e450a893190d0af3a
paulopariz <paulopariz01@gmail.com>
2024-02-15 21:04:19 -0300

    feat: validations

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,11 +1,35 @@
+import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
+import * as yup from "yup";
 
 interface ICity {
   name: string;
 }
 
-export const create = (req: Request<{}, {}, ICity>, res: Response) => {
-  console.log("data", req.body.name);
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+  name: yup.string().required().min(3),
+  state: yup.string().required().min(3),
+});
 
-  return res.send("Create!");
+export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
+  let validatedData: ICity | undefined = undefined;
+  try {
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+  }
+
+  console.log("va", validatedData);
+
+  return res.send("Create");
 };
