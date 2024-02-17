8090f8dd02a396fdbf025fbc9b87daab834169cd
paulopariz <paulopariz01@gmail.com>
2024-02-17 20:50:24 -0300

    feat: updateById

---
+++ src/server/controllers/cities/UpdateById.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import * as yup from "yup";
+import { validation } from "../../shared/middlewares";
+import { StatusCodes } from "http-status-codes";
+
+interface IParamProps {
+  id?: number;
+}
+
+interface IBodyProps {
+  name: string;
+}
+
+export const updateByIdValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(
+    yup.object().shape({
+      name: yup.string().required().min(3),
+    })
+  ),
+
+  params: getSchema<IParamProps>(
+    yup.object().shape({
+      id: yup.number().required().integer().moreThan(0),
+    })
+  ),
+}));
+
+export const updateById = async (
+  req: Request<IParamProps, {}, IBodyProps>,
+  res: Response
+) => {
+  console.log(req.params);
+  console.log(req.body);
+
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Nãp implementado");
+};
--- src/server/controllers/cities/index.ts
+++ src/server/controllers/cities/index.ts
@@ -1,9 +1,11 @@
 import * as create from "./Create";
 import * as getAll from "./GetAll";
 import * as getById from "./GetById";
+import * as updateById from "./UpdateById";
 
 export const CitiesController = {
   ...create,
   ...getAll,
   ...getById,
+  ...updateById,
 };
