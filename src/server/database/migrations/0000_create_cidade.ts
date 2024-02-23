02ff48d17b72d7027381e999f00e35eec4c09d47
paulopariz <paulopariz01@gmail.com>
2024-02-22 22:25:39 -0300

    config: migrate and database

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+import { ETableNames } from "../ETableName";
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETableNames.cidade, (table) => {
+      table.bigIncrements("id").primary().index();
+      table.string("nome", 150).index().notNullable();
+      table.comment("Tabela para armazenar cidades do sistema");
+    })
+    .then(() => {
+      console.log(`# Created tables ${ETableNames.cidade}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.cidade).then(() => {
+    console.log(`# Dropped tables ${ETableNames.cidade}`);
+  });
+}
