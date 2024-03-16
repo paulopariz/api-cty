fad6867ccddf0c2ee2010006760ac01a678451f8
paulopariz <paulopariz01@gmail.com>
2024-03-16 17:35:45 -0300

    feat: config cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import "dotenv/config";
 
 import "./shared/services/TranslationsYup";
@@ -6,7 +8,13 @@ import { router } from "./routes";
 
 const server = express();
 
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  })
+);
 server.use(express.json());
+
 server.use(router);
 
 export { server };
