3d51b2690486d9a99053dc2572a55af17cf0ec1e
paulopariz <paulopariz01@gmail.com>
2024-04-23 23:10:23 -0300

    fix

--- src/server/database/seeds/0000_insert_jobs.ts
+++ src/server/database/seeds/0000_insert_jobs.ts
@@ -2,13 +2,11 @@ import { ETableNames } from "../ETableName";
 import { Knex } from "knex";
 
 export const seed = async (knex: Knex) => {
-  const [{ count }] = await knex(ETableNames.job).count<[{ count: number }]>(
-    "* as count"
-  );
+  const [{ count }] = await knex(ETableNames.job).count<[{ count: number }]>("* as count");
   if (!Number.isInteger(count) || Number(count) > 0) return;
 
   const jobsToInsert = jobsAll.map((nameJob) => ({
-    name: nameJob,
+    title: nameJob,
   }));
   await knex(ETableNames.job).insert(jobsToInsert);
 };
