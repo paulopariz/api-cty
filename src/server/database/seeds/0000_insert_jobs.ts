6a09ab411c0de386d62dcdd233dcc48e3bc184af
paulopariz <paulopariz01@gmail.com>
2024-05-05 21:56:09 -0300

    fix

--- src/server/database/seeds/0000_insert_jobs.ts
+++ src/server/database/seeds/0000_insert_jobs.ts
@@ -10,12 +10,14 @@ export const seed = async (knex: Knex) => {
     description:
       "Buscamos Desenvolvedor Fullstack com forte experiência em PHP e Laravel. Conhecimentos em JavaScript, Vue.js e bases de dados são essenciais. Procuramos profissionais que possam gerenciar projetos de ponta a ponta, com foco em inovação e soluções escaláveis.",
     urgency_level: "urgent",
-    languages: "['php', 'laravel', 'javascript', 'vue']",
+    languages: JSON.stringify(["php", "laravel", "javascript", "vue"]),
     location_type: "remote",
-    labels: "['Fullstack', 'PHP', 'Laravel', 'JavaScript']",
+    labels: JSON.stringify(["Fullstack", "PHP", "Laravel", "JavaScript"]),
     contact: "4499941525512",
     status: "opened",
   };
+
+  console.log("jobsToInsert", jobsToInsert);
 
   await knex(ETableNames.job).insert(jobsToInsert);
 };
